669783f9e838e75784f0a46c53c036cc44cd8f7d
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-14 09:06:15 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  //grab all the necessary DOM elements
+  
 
-  //form and relevant input fields
+s
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
   const newTaskPriority = document.getElementById("new-task-priority");
 
-  //ul where new tasks will live on the DOM
+
   const newTaskUl = document.getElementById("tasks");
 
-  //attach event listeners
+
   newTaskForm.addEventListener("submit", createNewTask);
 });
 
 const createNewTask = event => {
   event.preventDefault();
-  //stop form from trying to submit
+
   const newTaskDescription = document.getElementById("new-task-description");
   const newTask = document.createElement("li");
   newTask.innerText = newTaskDescription.value;
